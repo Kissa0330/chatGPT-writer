88d4597227a2cf592fa0f062b1bc331efc339246
らいか <raika4789@gmail.com>
2023-03-14 20:18:24 +0900

    Google Analyticsを導入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
+import GoogleAnalytics from '../components/GoogleAnalytics'
 import '../styles/globals.css'
 import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GoogleAnalytics />
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default appWithTranslation(MyApp);
