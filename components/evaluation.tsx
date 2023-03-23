aea115b35f4aefcf002e545a27bb40e7b95e045d
らいか <raika4789@gmail.com>
2023-03-23 23:31:45 +0900

    useEffectが複数回実行されないように変更

--- components/evaluation.tsx
+++ components/evaluation.tsx
@@ -13,67 +13,65 @@ const App = (props: Prop) => {
 	const [isLoaded, setIsLoaded] = useState<boolean>(false);
 	const locale = useLocale();
 	const t = locale.t;
-  
+
 	async function submitGPT() {
 		setGptRes("");
 		let content = "";
-		if (locale.locale === "ja")
-		{
+		if (locale.locale === "ja") {
 			content += "以下の文章の一貫性を100点満点で評価してください。点数と評価を出力してください。点数は数字のみ出力してください。\n\""
 		}
-		else if (locale.locale === "en")
-		{
-			content += "Please rate the consistency of the following sentences on a 100-point scale. Please output the score and the rating. Please output only numbers for the score.\n\""
+		else if (locale.locale === "en") {
+			content += "Please rate the consistency of the following sentences on a 100-point scale. Please output the score and the evaluation. Please output only numbers for the score.\n\""
 		}
 		content += props.text;
 		content += "\""
 		const body = JSON.stringify({
-			content:content
+			content: content
 		});
 		const response = await fetch("/api/generalGPT", {
-		  method: "POST",
-		  headers: {
-			"Content-Type": "application/json",
-		  },
-		  body: body,
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: body,
 		});
 		setIsLoaded(true);
 		const data = response.body;
 		if (!data) {
-		  return;
+			return;
 		}
 		const reader = data.getReader();
 		const decoder = new TextDecoder();
 		let done = false;
-	
+
 		while (!done) {
-		  const { value, done: doneReading } = await reader.read();
-		  done = doneReading;
-		  const chunkValue = decoder.decode(value);
-		  setGptRes((prev) => prev + chunkValue);
+			const { value, done: doneReading } = await reader.read();
+			done = doneReading;
+			const chunkValue = decoder.decode(value);
+			setGptRes((prev) => prev + chunkValue);
 		}
 		setIsLoaded(false);
 	}
 	function gptResponseElements() {
 		const gptResArr = gptRes.split("\n\n");
 		const texts = gptResArr.map((item, index) => {
-		  return (
-			<Text className={styles.res_text} key={index}>
-			  {item}
-			</Text>
-		  );
+			return (
+				<Text className={styles.res_text} key={index}>
+					{item}
+				</Text>
+			);
 		});
 		return <div className={styles.gptRes}>{texts}</div>;
-	  }
-	useEffect(()=> {
+	}
+	useEffect(() => {
 		submitGPT();
-	},[props.text]);
+	}, []);
 	return (
 		<div>
-		<Text h3>
-			{t.evaluation.title}
-		</Text>
-		{gptResponseElements()}
+			<Text h3>
+				{t.evaluation.title}
+			</Text>
+			{gptResponseElements()}
 		</div>
 	)
 }
