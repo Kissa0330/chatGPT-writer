283d83aeb94da4cf0a22ee96ed846dd6beb3a561
らいか <raika4789@gmail.com>
2023-04-05 02:26:05 +0900

    評価ボタンとファクトチェックボタンを追加

--- pages/editor.tsx
+++ pages/editor.tsx
@@ -1,16 +1,25 @@
-import React, { useState } from 'react'
-import { Text } from '@nextui-org/react';
+import React, { useState, useRef } from 'react'
+import { useLocale } from "../hooks/useLocale";
+import { Button } from '@nextui-org/react';
 import { createEditor } from 'slate'
 import Head from "next/head";
 import { useRouter } from 'next/router';
 import { Slate, Editable, withReact } from 'slate-react'
 import styles from "../styles/Editor.module.css";
-// import Evaluation from "../components/evaluation"
-// import FactCheck from "../components/factcheck"
+import Evaluation from "../components/evaluation"
+import FactCheck from "../components/factcheck"
 
 const App = () => {
 	const { query, isReady } = useRouter()
 	const [editor] = useState(() => withReact(createEditor()))
+	const [evaluationText, setEvaluationText] = useState("");
+	const [isEvaluation, setIsEvaluation] = useState(false);
+	const [factCheckText, setFactCheckText] = useState("");
+	const [isFactCheck, setIsFactCheck] = useState(false);
+	const editorText = useRef("");
+	const locale = useLocale();
+	const t = locale.t;
+
 	if (!isReady) {
 		return;
 	}
@@ -22,8 +31,8 @@ const App = () => {
 				type: "paragrah",
 				children: [{ text: i }]
 			});
+			editorText.current += i;
 		}
-		console.log(textList);
 	}
 	const initialValue = textList
 	return (
@@ -38,11 +47,41 @@ const App = () => {
 				/>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<Text h1 className={styles.title}>Editor</Text>
-			<div className={styles.editor_wrapper}>
-				<Slate editor={editor} value={initialValue}>
-					<Editable />
-				</Slate>
+			<div className={styles.flex_wrapper}>
+				<div className={styles.editor_wrapper}>
+					<Slate editor={editor} value={initialValue} onChange={value => {
+						const isAstChange = editor.operations.some(
+							op => 'set_selection' !== op.type
+						)
+						if (isAstChange) {
+							editorText.current = "";
+							for (let i of value) {
+								for (let j of i.children) {
+									editorText.current += j.text;
+								}
+							}
+						}
+					}}>
+						<Editable />
+					</Slate>
+				</div>
+				<div className={styles.flex_left}>
+					<div className={styles.flex_left_wrapper}>
+						{isEvaluation && <Evaluation text={evaluationText} />}
+						<Button className={styles.flex_left_button}
+							onClick={() => {
+							setIsEvaluation(true);
+							setEvaluationText(editorText.current)
+						}}>{t.evaluation.title}</Button>
+					</div>
+					<div className={styles.flex_left_wrapper}>
+						{isFactCheck && <FactCheck text={factCheckText} />}
+						<Button className={styles.flex_left_button} onClick={() => {
+							setIsFactCheck(true);
+							setFactCheckText(editorText.current)
+						}}>{t.factcheck.title}</Button>
+					</div>
+				</div>
 			</div>
 		</>
 	)
